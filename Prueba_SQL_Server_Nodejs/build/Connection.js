9c95840f3d6411f9236aad228f64cef71e170c6b
Pedro Ordoñez <48272984+Pegre369@users.noreply.github.com>
2021-07-08 15:23:28 -0600

    Create  example

---
+++ Prueba_SQL_Server_Nodejs/build/Connection.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+    In this file we will have the connection to SQL-server, first will need a constant,
+    that constant have a new requirement that will is rest-mssql-nodejs.
+    (you will need to install rest-mssql-nodejs with the command "npm install rest-mssql-nodejs")
+ */
+const rest = new (require('rest-mssql-nodejs'))({
+    user: 'Pedro',
+    password: '1234',
+    database: 'Pro1',
+    server: 'DESKTOP-72T5476',
+    port: 1433,
+    options: {
+        encrypt: true // this is optional, by default is false
+    }
+});
+module.exports = {
+    rest
+};
--- Prueba_SQL_Server_Nodejs/build/index.js
+++ Prueba_SQL_Server_Nodejs/build/index.js
@@ -6,29 +6,41 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
-const apiroutes_1 = __importDefault(require("./routes/apiroutes"));
-class Servidor {
+const apiroutes_1 = __importDefault(require("./routes/apiroutes")); //In this part, import the file that manage the petitions
+class Server {
     constructor() {
         this.app = express_1.default();
         this.config();
         this.routes();
     }
     config() {
-        this.app.set('port', process.env.PORT || 9000);
+        this.app.set('port', process.env.PORT || 9000); //This is the number of the port, you can change that number 
         this.app.use(morgan_1.default('dev'));
         this.app.use(express_1.default.json());
         this.app.use(cors_1.default());
         this.app.use(express_1.default.urlencoded({ extended: false }));
     }
+    /*In this method, we will write the different routs that your server will use.
+    If you will use a rout that don't have a specific name, you can put only this symbol '/' in the rout name.
+    But if you will  manage a different routs or controllers, I recomend that your rout begins with something
+    that give an idea that what will you need or how will you divices your petitions.
+
+    For Example:
+
+    if you will have a route, where that rout only return a specific information, something like a user nickname and your password,
+    you can use '/api' that rout will have all peticion that only give specific information.
+    
+    It is just a recommendation, you can use the path of the same name for everyone,
+    but in the next part you should put something that differentiates your petitions.
+    */
     routes() {
-        //this.app.use('/prueba', prueba);
         this.app.use('/api', apiroutes_1.default);
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
-            console.log('servidor en puerto ' + this.app.get('port'));
+            console.log('This server listen on port ' + this.app.get('port'));
         });
     }
 }
-const server = new Servidor();
+const server = new Server();
 server.start();
